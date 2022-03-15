8f93e88489cd5896911b8611756a1c268cbc099c
Eloi-Perez <eloi104@hotmail.com>
2022-03-15 19:06:28 +0000

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+
+
+const DB_ITEMS = {
+  a: { price: 0.5, offer: { items: 3, price: 1.3 } },
+  b: { price: 0.3, offer: { items: 2, price: 0.45 } },
+  c: { price: 0.2 },
+  d: { price: 0.15 }
+}
+
 export default function App() {
   return (
     <div className="App">
